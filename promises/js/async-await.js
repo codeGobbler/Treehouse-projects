d23106cf2c1fd57b67b820cf166aa74ef2ce9590
Alton Michaux <alton.michaux@yahoo.com>
2021-01-21 21:01:49 -0500

    updated how many people in space project to use async-await functions

--- promises/js/async-await.js
+++ promises/js/async-await.js
@@ -1,27 +1,48 @@
-const astrosUrl = 'http://api.open-notify.org/astros.json';
-const wikiUrl = 'https://en.wikipedia.org/api/rest_v1/page/summary/';
-const peopleList = document.getElementById('people');
-const btn = document.querySelector('button');
+const astrosUrl = "http://api.open-notify.org/astros.json";
+const wikiUrl = "https://en.wikipedia.org/api/rest_v1/page/summary/";
+const peopleList = document.getElementById("people");
+const btn = document.querySelector("button");
 
 // Handle all fetch requests
+async function getPeopleInSpace(url) {
+  const peopleResponse = await fetch(url);
+  const peopleJSON = await peopleResponse.json();
+  // console.log(peopleJSON)
+  const profiles = peopleJSON.people.map(async (person) => {
+    const craft = person.craft;
+    const profileResponse = await fetch(wikiUrl + person.name);
+    const profileJSON = await profileResponse.json();
 
+    return { ...profileJSON, craft };
+  });
+  return Promise.all(profiles);
+}
 
 // Generate the markup for each profile
 function generateHTML(data) {
-  data.map( person => {
-    const section = document.createElement('section');
-    peopleList.appendChild(section);
-    section.innerHTML = `
+  // console.log(data);
+  data.map((person) => {
+    // console.log(person);
+    if (person.type === "standard") {
+      const section = document.createElement("section");
+      peopleList.appendChild(section);
+      section.innerHTML = `
       <img src=${person.thumbnail.source}>
       <span>${person.craft}</span>
       <h2>${person.title}</h2>
       <p>${person.description}</p>
       <p>${person.extract}</p>
     `;
+    }
   });
 }
 
-btn.addEventListener('click', (event) => {
+btn.addEventListener("click", async (event) => {
   event.target.textContent = "Loading...";
 
+  const astros = await getPeopleInSpace(astrosUrl);
+  // console.log(astros)
+  generateHTML(astros);
+
+  event.target.remove();
 });
