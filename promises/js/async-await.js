da6e0f1aa1eea33251a4a85df81ee606db8580bb
Alton Michaux <alton.michaux@yahoo.com>
2021-01-22 16:46:12 -0500

    finished up people in space project

--- promises/js/async-await.js
+++ promises/js/async-await.js
@@ -3,15 +3,22 @@ const wikiUrl = "https://en.wikipedia.org/api/rest_v1/page/summary/";
 const peopleList = document.getElementById("people");
 const btn = document.querySelector("button");
 
+async function getJSON(url) {
+  try {
+    const response = await fetch(url)
+    return await response.json();
+  } catch (error) {
+    throw error;
+  }
+}
+
 // Handle all fetch requests
 async function getPeopleInSpace(url) {
-  const peopleResponse = await fetch(url);
-  const peopleJSON = await peopleResponse.json();
+  const peopleJSON = await getJSON(url);
   // console.log(peopleJSON)
   const profiles = peopleJSON.people.map(async (person) => {
     const craft = person.craft;
-    const profileResponse = await fetch(wikiUrl + person.name);
-    const profileJSON = await profileResponse.json();
+    const profileJSON = await getJSON(wikiUrl + person.name);
 
     return { ...profileJSON, craft };
   });
@@ -37,12 +44,15 @@ function generateHTML(data) {
   });
 }
 
-btn.addEventListener("click", async (event) => {
+btn.addEventListener("click", (event) => {
   event.target.textContent = "Loading...";
 
-  const astros = await getPeopleInSpace(astrosUrl);
-  // console.log(astros)
-  generateHTML(astros);
-
-  event.target.remove();
+  getPeopleInSpace(astrosUrl)
+    .then(generateHTML)
+    .catch( e => {
+      peopleList.innerHTML = '<h3>Something went wrong</h3>'
+      console.error(e)
+    })
+    .finally( event.target.remove() )
+  
 });
